df94647758b1a94150a5bf83bb873887b0ea41c6
BENISXELLI <mirko.beniscelli@gmail.com>
2025-09-10 03:41:51 -0300

    segunda prueba

--- frontend/app/Config/Routes.php
+++ frontend/app/Config/Routes.php
@@ -5,5 +5,5 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');        // Página principal
+$routes->get('/', 'empanadas::index');        // Página principal
 $routes->get('/empanadas', 'Empanadas::index');  // Lista de empanadas
